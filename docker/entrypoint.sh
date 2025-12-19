d3486ba9a7a5239d21249e02727dac4ded5e25bc
Nils Bosbach <bosbach@ice.rwth-aachen.de>
2025-12-19 15:56:52 +0100

    create archive for images and configs
    Signed-off-by: Nils Bosbach <bosbach@ice.rwth-aachen.de>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -35,27 +35,31 @@ fi
 cd /app/project/app
 
 image_dir=/app/images
+tmp_image_dir=/app/project/tmp_images
 cores=(1 2 4 8)
 apps=()
 for dir in app/*/; do
     apps+=("$(basename "$dir")")
 done
+mkdir -p ${tmp_image_dir}
 
 for app in ${apps[@]}; do
-    mkdir -p ${image_dir}/${app}
-    sed -e "s/%NAME%/${app}/g" /app/project/app/vp_config/zephyr.cfg >  ${image_dir}/${app}/${app}.cfg
+    mkdir -p ${tmp_image_dir}/${app}
+    sed -e "s/%NAME%/${app}/g" /app/project/app/vp_config/zephyr.cfg >  ${tmp_image_dir}/${app}/${app}.cfg
+
     for core in "${cores[@]}"; do
         build_dir=/app/project/build/${app}x${core}
         mkdir -p ${build_dir}
         west build -b avp64/avp64/x${core} app/${app} -d ${build_dir} -- -DSOC_ROOT="$PWD" -DBOARD_ROOT="$PWD"
 
-        cp ${build_dir}/zephyr/zephyr.bin ${image_dir}/${app}/${app}x${core}.bin
-        cp ${build_dir}/zephyr/zephyr.elf ${image_dir}/${app}/${app}x${core}.elf
+        cp ${build_dir}/zephyr/zephyr.bin ${tmp_image_dir}/${app}/${app}x${core}.bin
+        cp ${build_dir}/zephyr/zephyr.elf ${tmp_image_dir}/${app}/${app}x${core}.elf
 
         cfg_file="system.desc = Boots ${app} on a ${core}-core AVP64\n"
         cfg_file+="system.cpu.ncores = ${core}\n"
         cfg_file+="%include \${dir}/${app}/${app}.cfg\n"
-        echo -e "${cfg_file}" > ${image_dir}/${app}x${core}.cfg
-
+        echo -e "${cfg_file}" > ${tmp_image_dir}/${app}x${core}.cfg
     done
+    tar -czf ${image_dir}/${app}.tar.gz -C ${tmp_image_dir} .
+    rm -rf ${tmp_image_dir}/*
 done
